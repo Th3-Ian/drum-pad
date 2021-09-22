ec7e405ad3724994cbdfeb2f2412d6c4f32c2440
Ian Hart <ianhart13@gmail.com>
2021-09-21 22:24:31 -0400

    add js funcs to play sounds & toggle css transitions

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // app.js
 
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+
+	console.log('transform')
+};
+
 window.addEventListener('keydown', function (e) {
-	console.log(e)
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const pad = document.querySelector(`.drum-pad[data-key="${e.keyCode}"]`);
+	if(!audio) return;
+
+	pad.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
 });
+
+const keys = Array.from(document.querySelectorAll('.drum-pad'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
